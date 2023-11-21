3b97695cab5a8a8b025b14d2a2c7ac79e5f0a1e4
Kobby7777 <godfredmirekuowusu@gmail.com>
2023-11-21 04:22:23 +0000

    completed the delete functionality

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -1,10 +1,23 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { MdOutlineClose } from "react-icons/md";
-
-const DeleteBlock = () => {
+const DeleteBlock = ({ id }) => {
+  const router = useRouter();
+  const deleteTicket = async () => {
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+      method: "DELETE",
+    });
+    if (res.ok) {
+      router.refresh();
+    }
+  };
   return (
     <div>
-      <MdOutlineClose className="text-red-400 hover:cursor-pointer hover:text-red-200 " />
+      <MdOutlineClose
+        className="text-red-400 hover:cursor-pointer hover:text-red-200 "
+        onClick={deleteTicket}
+      />
     </div>
   );
 };
